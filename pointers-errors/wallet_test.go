dcf6f0d9c9826084f3b4897d5b30dd7a86c77ef8
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-09 21:21:13 +0800

    Add additional checks in assertError

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -14,10 +14,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assetError := func(t testing.TB, err error) {
+	assertError := func(t testing.TB, got error, want string) {
 		t.Helper()
-		if err == nil {
+		if got == nil {
 			t.Error("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
@@ -40,7 +44,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assetError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 
 	})
 }

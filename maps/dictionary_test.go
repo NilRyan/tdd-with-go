d156d7d469dc465d48598096776876d961eaeaca
NilRyan <neilryanlipaod@gmail.com>
2022-05-10 17:41:10 +0800

    Add test for unknown words in Search

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,27 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got := dictionary.Search(dictionary, "test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+
+	})
+
+	t.Run("uknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 
 }
 

349aff7c261663d2a8a8a6824ace71cb9cfefed0
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-13 19:07:22 +0800

    Modify Countdown implementation to make test pass

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -13,23 +13,22 @@ const countdownStart = 3
 type Sleeper interface {
 	Sleep()
 }
-type SpySleeper struct {
-	Calls int
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
